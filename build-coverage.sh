d1cb8c532e209fd561b55c57e690501426f6b671
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-03-20 16:29:59 +0800

    update build-coverage.sh

--- build-coverage.sh
+++ build-coverage.sh
@@ -19,55 +19,37 @@ jobs=${JOBS:-$(get_jobs)}
 
 cd "$vbox_dir"
 
-out_base_dir=${OUT_BASE_DIR:-"$vbox_dir/out-clang-coverage"}
-mkdir -p -- "$out_base_dir"
-out_base_dir=$(readlink -f -- "$out_base_dir")
-profile_dir=${PROFILE_DIR:-"$out_base_dir/profiles"}
-mkdir -p -- "$profile_dir"
+out_base_dir=$vbox_dir/out-clang-coverage
+mkdir -p $out_base_dir
 
 ./configure \
     --disable-hardening \
-    -d \
     --disable-docs \
     --disable-java \
     --disable-qt \
     --build-headless \
-    --disable-kmods \
     --nofatal \
     --out-base-dir="$out_base_dir"
 
 # shellcheck disable=SC1090
 source "$out_base_dir/env.sh"
 
-tool_clang_cc=${TOOL_CLANG_CC:-"clang -m64 -fprofile-instr-generate -fcoverage-mapping"}
-tool_clang_cxx=${TOOL_CLANG_CXX:-"clang++ -m64 -fprofile-instr-generate -fcoverage-mapping"}
-tool_clang_ld=${TOOL_CLANG_LD:-"clang++ -m64 -fprofile-instr-generate -fcoverage-mapping"}
-
-have_explicit_target=0
-for arg in "$@"; do
-    case "$arg" in
-        -*|*=*)
-            ;;
-        *)
-            have_explicit_target=1
-            ;;
-    esac
-done
-
-if [ $# -eq 0 ] || [ "$have_explicit_target" -eq 0 ]; then
-    set -- "$@" VBoxHeadless VBoxManage
-fi
-
 kmk_args=(
     VBOX_GCC_TOOL=CLANG
-    "TOOL_CLANG_CC=$tool_clang_cc"
-    "TOOL_CLANG_CXX=$tool_clang_cxx"
-    "TOOL_CLANG_LD=$tool_clang_ld"
-    "-j${jobs}"
+    VBOX_SVN_REV=172322
+    'TOOL_CLANG_CFLAGS+= -fprofile-instr-generate -fcoverage-mapping'
+    'TOOL_CLANG_CXXFLAGS+= -fprofile-instr-generate -fcoverage-mapping'
 )
 
-kmk "${kmk_args[@]}" "$@"
+kmk "${kmk_args[@]}" 
 
-echo "coverage build output: $out_base_dir"
-echo "set LLVM_PROFILE_FILE to collect profiles, for example:"
-echo "  LLVM_PROFILE_FILE=$profile_dir/%p-%m.profraw"
+cd out-clang-coverage/linux.amd64/release/bin/src
+sudo make 
+sudo make install
+sudo rmmod vboxnetadp vboxnetflt vboxdrv
+sudo insmod vboxdrv.ko
+sudo insmod vboxnetflt.ko
+sudo insmod vboxnetadp.ko
+sudo chmod o+rw /dev/vboxdrv
+cd -
+
